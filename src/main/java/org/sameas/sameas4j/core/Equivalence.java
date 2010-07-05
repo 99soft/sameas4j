32b40694f335189519e50657c6de67d8d72ec569
simonetripodi <simone.tripodi@gmail.com>
2010-07-05 14:26:11 +0000

    fixed code comment
    git-svn-id: http://asmx-sameas4j.googlecode.com/svn/trunk/sameas-core@91 33b2e748-5b9f-11de-a2a3-1fafad8a8996

--- src/main/java/org/sameas/sameas4j/core/Equivalence.java
+++ src/main/java/org/sameas/sameas4j/core/Equivalence.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 /**
  * This class models an equivalence. An equivalence is a response given by
- * <a href="http://sameas4j.org">sameas4j.org</a> to a given <code>URI</code> in
+ * <a href="http://sameas.org">sameas.org</a> to a given <code>URI</code> in
  * terms of other <code>owl:sameAs</code> <code>URI</code>.
  *
  * @author Davide Palmisano (dpalmisano@gmail.com)
