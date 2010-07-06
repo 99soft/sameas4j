d4dbddb3f7d3410c11e62ffb6e19016cae7eddf5
simonetripodi <simone.tripodi@gmail.com>
2010-07-06 15:50:25 +0000

    minor javadoc
    git-svn-id: http://asmx-sameas4j.googlecode.com/svn/trunk/sameas-core@122 33b2e748-5b9f-11de-a2a3-1fafad8a8996

--- src/main/java/org/sameas/sameas4j/SameAsService.java
+++ src/main/java/org/sameas/sameas4j/SameAsService.java
@@ -2,7 +2,6 @@ package org.sameas.sameas4j;
 
 import java.net.URI;
 
-
 /**
  * It models the methods that a <a href="http://sameas.org">sameas.org</a>
  * client must expose.
@@ -16,9 +15,9 @@ public interface SameAsService {
      * It returns a set of <code>URI</code> identifying equivalent
      * resources to the input <code>URI</code>.
      *
-     * @param uri
-     * @return
-     * @throws SameAsServiceException
+     * @param uri the URI for which the equivalence has to be known.
+     * @return the URI equivalence.
+     * @throws SameAsServiceException if an HTTP or JSON parse error occurs.
      */
     Equivalence getDuplicates(URI uri) throws SameAsServiceException;
 
