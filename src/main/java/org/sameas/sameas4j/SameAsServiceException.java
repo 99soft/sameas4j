ccbf74c306ebcb187f0ca1a001a95a599a457fff
simonetripodi <simone.tripodi@gmail.com>
2010-07-06 15:52:19 +0000

    minor javadoc
    git-svn-id: http://asmx-sameas4j.googlecode.com/svn/trunk/sameas-core@123 33b2e748-5b9f-11de-a2a3-1fafad8a8996

--- src/main/java/org/sameas/sameas4j/SameAsServiceException.java
+++ src/main/java/org/sameas/sameas4j/SameAsServiceException.java
@@ -1,19 +1,33 @@
 package org.sameas.sameas4j;
 
 /**
- * Raised when an error occours within {@link org.sameas.sameas4j.SameAsService}.
+ * Raised when an error occurs within {@link org.sameas.sameas4j.SameAsService}.
  *
  * @author Davide Palmisano (dpalmisano@gmail.com)
  * @version $Id$
  */
 public final class SameAsServiceException extends Exception {
 
+    /**
+     * The default serialVersionUID.
+     */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Constructs a new SameAsServiceException with the specified detail message.
+     *
+     * @param message the specified detail message.
+     */
     public SameAsServiceException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param message the specified detail message.
+     * @param e the specified detail cause.
+     */
     public SameAsServiceException(String message, Exception e) {
         super(message, e);
     }
