01d0ea6c6358f90cf992932a9f4d57789317c395
simonetripodi <simone.tripodi@gmail.com>
2010-07-06 15:47:20 +0000

    minor javadoc
    git-svn-id: http://asmx-sameas4j.googlecode.com/svn/trunk/sameas-core@120 33b2e748-5b9f-11de-a2a3-1fafad8a8996

--- src/main/java/org/sameas/sameas4j/Equivalence.java
+++ src/main/java/org/sameas/sameas4j/Equivalence.java
@@ -15,41 +15,73 @@ import java.util.Set;
  */
 public final class Equivalence {
 
+    /**
+     * The equivalent URIs.
+     */
     private final Set<URI> duplicates = new HashSet<URI>();
 
+    /**
+     * The requested URI.
+     */
     private final URI uri;
 
+    /**
+     * Creates a new {@code Equivalence} instance by the requested URI.
+     *
+     * @param uri the requested URI.
+     */
     public Equivalence(URI uri) {
         this.uri = uri;
     }
 
+    /**
+     * Returns the requested URI.
+     *
+     * @return the requested URI.
+     */
     public URI getUri() {
         return this.uri;
     }
 
     /**
-     * Returns the number of equivalent URIs
+     * Returns the number of equivalent URIs.
      *
-     * @return an integer
+     * @return the number of equivalent URIs.
      */
     public int getAmount() {
         return this.duplicates.size();
     }
 
+    /**
+     * Returns the equivalent URIs data structure.
+     *
+     * @return the equivalent URIs data structure.
+     */
     public Iterable<URI> getDuplicates() {
         return Collections.unmodifiableSet(this.duplicates);
     }
 
+    /**
+     * Adds an equivalent URI.
+     *
+     * @param uri an equivalent URI.
+     */
     protected void addDuplicate(URI uri) {
         this.duplicates.add(uri);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return String.format("Equivalence { uri: %s, amount: %s, duplicates: %s }",
                 this.uri, this.getAmount(), this.duplicates);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object object) {
         if (this == object) {
@@ -61,6 +93,9 @@ public final class Equivalence {
         return this.uri.equals(((Equivalence) object).getUri()) ;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return this.uri.hashCode();
