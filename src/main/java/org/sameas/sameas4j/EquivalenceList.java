578ff1700b9aef1ee97c5f1557c1e2efc64441e2
simonetripodi <simone.tripodi@gmail.com>
2010-07-24 22:02:37 +0000

    added javadoc
    git-svn-id: http://asmx-sameas4j.googlecode.com/svn/trunk/sameas-core@132 33b2e748-5b9f-11de-a2a3-1fafad8a8996

--- src/main/java/org/sameas/sameas4j/EquivalenceList.java
+++ src/main/java/org/sameas/sameas4j/EquivalenceList.java
@@ -5,27 +5,46 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * Default implementation of {@link org.sameas.sameas4j.SameAsService}.
+ * Represents an {@link Equivalence} list.
  *
  * @version $Id$
  */
 public final class EquivalenceList implements Iterable<Equivalence> {
 
+    /**
+     * The {@link Equivalence} list.
+     */
     private final List<Equivalence> equivalenceList = new ArrayList<Equivalence>();
 
+    /**
+     * Add a new {@link Equivalence} into the list.
+     *
+     * @param equivalence the {@link Equivalence} has to be added into the list.
+     */
     protected void addEquivalence(Equivalence equivalence) {
         this.equivalenceList.add(equivalence);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Iterator<Equivalence> iterator() {
         return equivalenceList.iterator();
     }
 
+    /**
+     * Return the number of {@link Equivalence} found.
+     *
+     * @return the number of {@link Equivalence} found.
+     */
     public int size() {
         return this.equivalenceList.size();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return this.equivalenceList.toString();
