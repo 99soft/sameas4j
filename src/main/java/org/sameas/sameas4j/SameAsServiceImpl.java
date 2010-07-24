7d3d5f09695bf863cc5e13539fbf399b76a5831d
simonetripodi <simone.tripodi@gmail.com>
2010-07-24 21:14:20 +0000

    removed code duplicates
    git-svn-id: http://asmx-sameas4j.googlecode.com/svn/trunk/sameas-core@130 33b2e748-5b9f-11de-a2a3-1fafad8a8996

--- src/main/java/org/sameas/sameas4j/SameAsServiceImpl.java
+++ src/main/java/org/sameas/sameas4j/SameAsServiceImpl.java
@@ -42,65 +42,33 @@ final class SameAsServiceImpl implements SameAsService {
      */
     public SameAsServiceImpl() {
         this.gsonBuilder.registerTypeAdapter(Equivalence.class, new EquivalenceDeserializer());
-        this.gsonBuilder.registerTypeAdapter(List.class, new ListEquivalenceDeserializer());        
+        this.gsonBuilder.registerTypeAdapter(List.class, new ListEquivalenceDeserializer());
     }
 
     /**
      * {@inheritDoc}
      */
     public Equivalence getDuplicates(URI uri) throws SameAsServiceException {
-        Equivalence equivalence = null;
-
-        String toBeInvoked = String.format(SERVICE_URL, uri);
-        URL url;
-        try {
-            url = new URL(toBeInvoked);
-        } catch (MalformedURLException e) {
-            throw new SameAsServiceException("An error occurred while building the URL '"
-                    + toBeInvoked
-                    + "'");
-        }
-
-        URLConnection connection = null;
-        Reader reader = null;
-        try {
-            connection = url.openConnection();
-            if (connection instanceof HttpURLConnection) {
-                ((HttpURLConnection) connection).connect();
-            }
-
-            reader = new InputStreamReader(connection.getInputStream());
-            Gson gson = this.gsonBuilder.create();
-            equivalence = gson.fromJson(reader, Equivalence.class);
-        } catch (IOException e) {
-            throw new SameAsServiceException("An error occurred while invoking the URL '"
-                    + toBeInvoked
-                    + "'");
-        } catch (JsonParseException e) {
-            throw new SameAsServiceException("An error occurred while parsing the JSON response", e);
-        } finally {
-            if (connection != null && connection instanceof HttpURLConnection) {
-                ((HttpURLConnection) connection).disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    // close it quietly
-                }
-            }
-        }
-
-        return equivalence;
+        return invokeULR(String.format(SERVICE_URL, uri), Equivalence.class);
     }
 
     /**
      * {@inheritDoc}
      */
     public List<Equivalence> getDuplicates(String keyword) throws SameAsServiceException {
-        List<Equivalence> equivalences = null;
+        return invokeULR(String.format(SERVICE_KEYWORD, keyword), List.class);
+    }
 
-        String toBeInvoked = String.format(SERVICE_KEYWORD, keyword);
+    /**
+     * Invokes a Sameas.org service URL and parses the JSON response.
+     *
+     * @param <T> the expected return type.
+     * @param toBeInvoked the service URL has to be invoked.
+     * @param returnType the type the JSON response has to be bind to.
+     * @return the bound object.
+     * @throws SameAsServiceException is any error occurs.
+     */
+    private <T> T invokeULR(String toBeInvoked, Class<T> returnType) throws SameAsServiceException {
         URL url;
         try {
             url = new URL(toBeInvoked);
@@ -120,7 +88,7 @@ final class SameAsServiceImpl implements SameAsService {
 
             reader = new InputStreamReader(connection.getInputStream());
             Gson gson = this.gsonBuilder.create();
-            equivalences = (List<Equivalence>) gson.fromJson(reader, List.class);
+            return gson.fromJson(reader, returnType);
         } catch (IOException e) {
             throw new SameAsServiceException("An error occurred while invoking the URL '"
                     + toBeInvoked
@@ -139,7 +107,6 @@ final class SameAsServiceImpl implements SameAsService {
                 }
             }
         }
-        return equivalences;
     }
 
 }
