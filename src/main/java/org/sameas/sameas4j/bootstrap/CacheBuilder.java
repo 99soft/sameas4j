c89a800440631c37a9b5a0e4f598ddee79b80c36
simonetripodi <simone.tripodi@gmail.com>
2010-08-26 21:24:02 +0000

    first checkin of the builder API
    git-svn-id: http://asmx-sameas4j.googlecode.com/svn/trunk/sameas-core@151 33b2e748-5b9f-11de-a2a3-1fafad8a8996

---
+++ src/main/java/org/sameas/sameas4j/bootstrap/CacheBuilder.java
@@ -0,0 +1,29 @@
+package org.sameas.sameas4j.bootstrap;
+
+import org.sameas.sameas4j.SameAsService;
+import org.sameas.sameas4j.cache.Cache;
+import org.sameas.sameas4j.cache.InMemoryCache;
+
+/**
+ * 
+ * @since 1.2
+ * @version $Id$
+ */
+public final class CacheBuilder {
+
+    private final SameAsService sameAsService;
+
+    protected CacheBuilder(SameAsService sameAsService) {
+        this.sameAsService = sameAsService;
+    }
+
+    public SameAsService usingDefaultInMemoryCache() {
+        return this.usingCache(new InMemoryCache());
+    }
+
+    public SameAsService usingCache(Cache cache) {
+        this.sameAsService.setCache(cache);
+        return this.sameAsService;
+    }
+
+}
---
+++ src/main/java/org/sameas/sameas4j/bootstrap/SameAsServiceBuilder.java
@@ -0,0 +1,25 @@
+package org.sameas.sameas4j.bootstrap;
+
+import org.sameas.sameas4j.DefaultSameAsServiceFactory;
+import org.sameas.sameas4j.SameAsService;
+
+/**
+ * 
+ * @since 1.2
+ * @version $Id$
+ */
+public final class SameAsServiceBuilder {
+
+    public CacheBuilder createNew() {
+        return this.newCacheBuilder(DefaultSameAsServiceFactory.createNew());
+    }
+
+    public CacheBuilder getSingletonInstance() {
+        return this.newCacheBuilder(DefaultSameAsServiceFactory.getSingletonInstance());
+    }
+
+    private CacheBuilder newCacheBuilder(SameAsService sameAsService) {
+        return new CacheBuilder(sameAsService);
+    }
+
+}
