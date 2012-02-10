0ff1ab2491eb2e35e78b323cae48cb93a851b360
simonetripodi <simone.tripodi@gmail.com>
2012-02-10 16:09:01 +0100

    updated copyright year

--- src/main/java/org/nnsoft/sameas4j/EquivalenceListDeserializer.java
+++ src/main/java/org/nnsoft/sameas4j/EquivalenceListDeserializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2011 The 99 Software Foundation
+ * Copyright (c) 2009-2012 The 99 Software Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
