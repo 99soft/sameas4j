4301a1e70969a1f53c32c25d0166de8217722d9a
simonetripodi <simone.tripodi@gmail.com>
2012-02-10 15:25:47 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,12 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.sameas4j", "org/nnsoft/sameas4j/package-summary.html", [ [ "Description", "org/nnsoft/sameas4j/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "SameAsService", "org/nnsoft/sameas4j/SameAsService.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "DefaultSameAsServiceFactory", "org/nnsoft/sameas4j/DefaultSameAsServiceFactory.html", null, "" ], [ "Equivalence", "org/nnsoft/sameas4j/Equivalence.html", null, "" ], [ "EquivalenceList", "org/nnsoft/sameas4j/EquivalenceList.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "SameAsServiceException", "org/nnsoft/sameas4j/SameAsServiceException.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.sameas4j.cache", "org/nnsoft/sameas4j/cache/package-summary.html", [ [ "Description", "org/nnsoft/sameas4j/cache/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "Cache", "org/nnsoft/sameas4j/cache/Cache.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "CacheKey", "org/nnsoft/sameas4j/cache/CacheKey.html", null, "" ], [ "InMemoryCache", "org/nnsoft/sameas4j/cache/InMemoryCache.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,13 @@
+var DATA = [
+      { id:0, label:"org.nnsoft.sameas4j", link:"org/nnsoft/sameas4j/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.sameas4j.DefaultSameAsServiceFactory", link:"org/nnsoft/sameas4j/DefaultSameAsServiceFactory.html", type:"class" },
+      { id:2, label:"org.nnsoft.sameas4j.Equivalence", link:"org/nnsoft/sameas4j/Equivalence.html", type:"class" },
+      { id:3, label:"org.nnsoft.sameas4j.EquivalenceList", link:"org/nnsoft/sameas4j/EquivalenceList.html", type:"class" },
+      { id:4, label:"org.nnsoft.sameas4j.SameAsService", link:"org/nnsoft/sameas4j/SameAsService.html", type:"class" },
+      { id:5, label:"org.nnsoft.sameas4j.SameAsServiceException", link:"org/nnsoft/sameas4j/SameAsServiceException.html", type:"class" },
+      { id:6, label:"org.nnsoft.sameas4j.cache", link:"org/nnsoft/sameas4j/cache/package-summary.html", type:"package" },
+      { id:7, label:"org.nnsoft.sameas4j.cache.Cache", link:"org/nnsoft/sameas4j/cache/Cache.html", type:"class" },
+      { id:8, label:"org.nnsoft.sameas4j.cache.CacheKey", link:"org/nnsoft/sameas4j/cache/CacheKey.html", type:"class" },
+      { id:9, label:"org.nnsoft.sameas4j.cache.InMemoryCache", link:"org/nnsoft/sameas4j/cache/InMemoryCache.html", type:"class" }
+
+    ];
