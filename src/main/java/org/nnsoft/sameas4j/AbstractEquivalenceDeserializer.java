91c407f3682ccc7e5179b157e09140fa36c72ead
simonetripodi <simone.tripodi@gmail.com>
2012-02-24 23:41:36 +0100

    added URL encoder to handle wrong char in responses

--- src/main/java/org/nnsoft/sameas4j/AbstractEquivalenceDeserializer.java
+++ src/main/java/org/nnsoft/sameas4j/AbstractEquivalenceDeserializer.java
@@ -21,8 +21,9 @@
  */
 package org.nnsoft.sameas4j;
 
+import java.io.ByteArrayOutputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.BitSet;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -50,6 +51,79 @@ abstract class AbstractEquivalenceDeserializer {
     private static final String EXCEPTION_MESSAGE = "URI '%s' seems to be not well-formed";
 
     /**
+     * The UTF-8 string constant.
+     */
+    private static final String UTF_8_ENCODING = "UTF-8";
+
+    /**
+     * BitSet of unreserved characters.
+     */
+    private static final BitSet UNRESERVED_CHARS = new BitSet(256);
+
+    /**
+     * The default escaping character of URL encoding.
+     */
+    private static final byte URL_ESCAPE_CHAR = '%';
+
+    static {
+        for (byte b = 'A'; b <= 'Z'; b++) {
+            UNRESERVED_CHARS.set(b);
+        }
+        for (byte b = 'a'; b <= 'z'; b++) {
+            UNRESERVED_CHARS.set(b);
+        }
+        for (byte b = '0'; b <= '9'; b++) {
+            UNRESERVED_CHARS.set(b);
+        }
+
+        // special URL encoding chars
+        UNRESERVED_CHARS.set('-');
+        UNRESERVED_CHARS.set('.');
+        UNRESERVED_CHARS.set('_');
+        UNRESERVED_CHARS.set('~');
+    }
+
+    /**
+     * Encodes the given text.
+     *
+     * @param text text to be encoded.
+     * @return encoding result.
+     */
+    public static String urlEncode(final String text) throws Exception {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        for (int b : toUTF8Bytes(text)) {
+            if (b < 0) {
+                b = 256 + b;
+            }
+
+            if (UNRESERVED_CHARS.get(b)) {
+                buffer.write(b);
+            } else {
+                buffer.write(URL_ESCAPE_CHAR);
+                char hex1 = Character.toUpperCase(Character.forDigit(
+                        (b >> 4) & 0xF, 16));
+                char hex2 = Character.toUpperCase(Character.forDigit(b & 0xF,
+                        16));
+                buffer.write(hex1);
+                buffer.write(hex2);
+            }
+        }
+
+        return new String(buffer.toByteArray());
+    }
+
+    /**
+     * Performs an UTF8 encoding.
+     *
+     * @param text text to be encoded.
+     * @return encoding result.
+     */
+    private static byte[] toUTF8Bytes(final String text) throws Exception {
+        return text.getBytes(UTF_8_ENCODING);
+    }
+
+    /**
      * Deserialize a single {@link org.nnsoft.sameas4j.Equivalence}
      * from its Json serialization.
      *
@@ -61,21 +135,26 @@ abstract class AbstractEquivalenceDeserializer {
         String uriString = json.getAsJsonObject().getAsJsonPrimitive(URI).getAsString();
         URI uri;
         try {
-            uri = new URI(uriString);
-        } catch (URISyntaxException e) {
+            uri = new URI(urlEncode(uriString));
+        } catch (Exception e) {
             throw new JsonParseException(String.format(EXCEPTION_MESSAGE, uriString));
         }
         equivalence = new Equivalence(uri);
         JsonArray duplicates = json.getAsJsonObject().getAsJsonArray(DUPLICATES);
         for (int i = 0; i < duplicates.size(); i++) {
             try {
-                equivalence.addDuplicate(new URI(duplicates.get(i).getAsString()));
-            } catch (URISyntaxException e) {
+                equivalence.addDuplicate(new URI(urlEncode(duplicates.get(i).getAsString())));
+            } catch (Exception e) {
                 // if an equivalent URI is not well-formed it's better to do not add it, let's go on
                 continue;
             }
         }
         return equivalence;
+    }
+
+    public static void main( String[] args ) throws Exception
+    {
+        System.err.println(urlEncode( "http://it.bestshopping.com/prezzi/Rome.sku=9780751339031|.html" ));
     }
 
 }
