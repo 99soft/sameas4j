b340d76eae171c53645a27c1b6ec57f446e85835
simonetripodi <simone.tripodi@gmail.com>
2012-02-24 23:53:47 +0100

    completed the set of reserved chars

--- src/main/java/org/nnsoft/sameas4j/AbstractEquivalenceDeserializer.java
+++ src/main/java/org/nnsoft/sameas4j/AbstractEquivalenceDeserializer.java
@@ -77,6 +77,14 @@ abstract class AbstractEquivalenceDeserializer {
         }
 
         // special URL encoding chars
+        UNRESERVED_CHARS.set('$');
+        UNRESERVED_CHARS.set('&');
+        UNRESERVED_CHARS.set('+');
+        UNRESERVED_CHARS.set(',');
+        UNRESERVED_CHARS.set(';');
+        UNRESERVED_CHARS.set('=');
+        UNRESERVED_CHARS.set('?');
+        UNRESERVED_CHARS.set('@');
         UNRESERVED_CHARS.set('/');
         UNRESERVED_CHARS.set(':');
         UNRESERVED_CHARS.set('-');
